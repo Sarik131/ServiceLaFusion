b37ffe841944cc0a6bac634f48cd56c67a69c13e
vinu129 <vinayvhc38@gmail.com>
2019-04-03 19:53:12 +0530

    by - Vinay

---
+++ app/src/main/java/com/shariq/service_lafusion/ServiceProvider.java
@@ -0,0 +1,13 @@
+package com.shariq.service_lafusion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceProvider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_provider);
+    }
+}
